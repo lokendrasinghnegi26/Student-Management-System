c1fb18c86be4ad20dca9df56bc188e39d2716f06
LokendraSinghNegi <61650132+LokendraSinghNegi@users.noreply.github.com>
2021-01-10 23:00:04 +0530

    Initial commit(using Firebase)

---
+++ app/src/main/java/com/lokendrasingh/welcome/Signup.java
@@ -0,0 +1,15 @@
+package com.lokendrasingh.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().setTitle("Signup");
+    }
+}
