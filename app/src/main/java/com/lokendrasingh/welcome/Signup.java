584e29a6c838db1af4d63a52e9c34cb103f0bcd6
LokendraSinghNegi <61650132+LokendraSinghNegi@users.noreply.github.com>
2021-01-12 22:32:11 +0530

    Add data in database(using firebase)

--- app/src/main/java/com/lokendrasingh/welcome/Signup.java
+++ app/src/main/java/com/lokendrasingh/welcome/Signup.java
@@ -10,18 +10,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup extends AppCompatActivity {
-    EditText txtemail, txtpassword, txtconfirmpassword;
+    EditText txtemail, txtpassword, txtconfirmpassword, name,father;
+    RadioButton gender_male, gender_female;
     Button btnRegister;
+    String Gender = "";
     ProgressBar progressBar;
     FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebasedatabase;      // initiliazing FirebaseDatabase but we can remove it
+     DatabaseReference databaseReference;       // initiliazing DatabaseReference
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +39,43 @@ public class Signup extends AppCompatActivity {
         txtconfirmpassword= findViewById(R.id.confirmpassword);
         btnRegister= findViewById(R.id.register);
         progressBar= findViewById(R.id.progressbar);
+        name= findViewById(R.id.name);
+        father= findViewById(R.id.father);
+        gender_female= findViewById(R.id.female);
+        gender_male= findViewById(R.id.male);
+        databaseReference= FirebaseDatabase.getInstance(). getReference("Student");
+
         firebaseAuth= FirebaseAuth.getInstance();       // we get the object of firebaseAuth
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String FullName         = name.getText().toString();
+                String fatherName       = father.getText().toString();
                 String email            = txtemail.getText().toString().trim();     // trim function will cancel the spaces
                 String password         = txtpassword.getText().toString().trim();
                 String confirmpassword  = txtconfirmpassword.getText().toString().trim();
+
+                if(gender_male.isChecked())
+                {
+                    Gender= "Male";
+                }
+
+                if(gender_female.isChecked())
+                {
+                    Gender= "Female";
+                }
+
+                if(TextUtils.isEmpty(FullName))                            // TextUtils is used for validation of TextView or EditText views
+                {
+                    Toast.makeText(Signup.this, "Enter Name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(fatherName))                            // TextUtils is used for validation of TextView or EditText views
+                {
+                    Toast.makeText(Signup.this, "Enter father's name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 if(TextUtils.isEmpty(email))                            // TextUtils is used for validation of TextView or EditText views
                 {
@@ -70,9 +107,26 @@ public class Signup extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     progressBar.setVisibility(View.GONE);
                                     if (task.isSuccessful()) {
+                                        student information= new student(
+                                                FullName,
+                                                fatherName,
+                                                email,
+                                                Gender
 
-                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                    Toast.makeText(Signup.this, "Registration complete",Toast.LENGTH_SHORT).show();
+                                        );
+
+                                        //passing path and object in database
+                                        FirebaseDatabase.getInstance().getReference("Student").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(information).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                                Toast.makeText(Signup.this, "Registration complete",Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                            }
+                                        });
+
+
+
 
                                     } else {
                                 Toast.makeText(Signup.this, "Authentication failed", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/lokendrasingh/welcome/student.java
@@ -0,0 +1,15 @@
+package com.lokendrasingh.welcome;
+
+public class student {
+    public String  name, father_name, email, gender;
+    public student()
+    {
+
+    }
+    public student(String name, String father_name, String email, String gender) {
+        this.name = name;
+        this.father_name = father_name;
+        this.email = email;
+        this.gender = gender;
+    }
+}
